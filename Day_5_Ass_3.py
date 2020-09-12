a1fb74b3178268529812301c4b1e314abfde91b4
Amit Pathak <55128822+Viking-Programmer@users.noreply.github.com>
2020-09-12 23:15:25 +0530

    Add files via upload

---
+++ Day_5_Ass_3.py
@@ -0,0 +1,26 @@
+class BankAccount:
+
+    def __init__(self, name, balance):
+        self.owner_name = name
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Amount successfully deposited. Current balance is Rs. {self.balance}.")
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+            print(f"Withdrawal successful. Current balance is Rs. {self.balance}.")
+        else:
+            print(f"Transaction could not be performed due to insufficient funds.")
+
+ba1 = BankAccount("Amit Pathak", 30000)
+ba1.deposit(4000)
+ba1.withdraw(20000)
+ba1.withdraw(20000)
+print("-"*30)
+ba2 = BankAccount("Bhavisha Kapadia", 10000)
+ba2.withdraw(12000)
+ba2.deposit(5000)
+ba2.withdraw(12000)
